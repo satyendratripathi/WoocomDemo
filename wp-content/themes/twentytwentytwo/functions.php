ee88a837b5a46ee996ac0c5ac6597ff18c6d83c0
satyendraadvatix <tripathi_satyendra@ymail.com>
2022-10-09 19:41:21 +0530

    enqueue woo-custom-style.css and Make add to cart button text bold and Custom plugin added in twentytwentytwo theme
    enqueue woo-custom-style.css and Make add to cart button text bold and Custom plugin added in twentytwentytwo theme

--- wp-content/themes/twentytwentytwo/functions.php
+++ wp-content/themes/twentytwentytwo/functions.php
@@ -111,9 +111,9 @@ function satyendra_custom_checkout_field_display_admin_order_meta($order){
 
 
 
-/**
- * Add a woo-custom-style.css
- */
+
+
+
 
 
 
@@ -173,6 +173,11 @@ if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
 
 		// Enqueue theme stylesheet.
 		wp_enqueue_style( 'twentytwentytwo-style' );
+		
+		
+ //  Adding Custom CSS file in theme woo-custom-style.css
+ 
+		wp_enqueue_style( 'woo-custom-style', get_template_directory_uri() . '/assets/css/woo-custom-style.css', array(), '1.1', 'all');
 
 	}
 
